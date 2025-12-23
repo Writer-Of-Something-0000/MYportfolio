dc62f00c4ee4f558d69a922cf4e811775273fea1
Just coder <138765040+Writer-Of-Something-0000@users.noreply.github.com>
2025-12-24 03:59:50 +0400

    clean website structure and write navigation

--- FrontPage/src/app/app-module.ts
+++ FrontPage/src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Navigation } from './navigation/navigation';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Navigation
   ],
   imports: [
     BrowserModule,
---
+++ FrontPage/src/app/navigation/navigation.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  standalone: false,
+  templateUrl: './navigation.html',
+  styleUrl: './navigation.css',
+})
+export class Navigation  {
+ isHover2 = false;
+ isFire = true;
+ isFire2 = false;
+ isFire3 = false;
+  // Texts
+  fullText = "Dark Mode";
+    fullText2 = "Luka Gengashvili";
+  displayedText2 = "";
+  index = 0;
+  typingForward = true; // true = typing, false = deleting
+  speed = 100; // typing speed in ms
+
+  // Displayed texts
+  displayedText = "";
+
+
+  // Intervals
+  typingInterval1: any;
+
+
+  // --- First typewriter ---
+  startTyping() {
+    this.isHover2 = true;
+    if (this.typingInterval1) return;
+
+    let index = 0;
+    this.typingInterval1 = setInterval(() => {
+      if (index < this.fullText.length) {
+        this.displayedText += this.fullText[index];
+        index++;
+      } else {
+        clearInterval(this.typingInterval1);
+        this.typingInterval1 = null;
+      }
+    }, 75);
+  }
+
+  resetTyping() {
+    clearInterval(this.typingInterval1);
+    this.typingInterval1 = null;
+    this.displayedText = "";
+  }
+
+  // --- Combined hover events ---
+  onHoverEnter() {
+    this.isHover2 = true;
+    this.startTyping();
+
+  }
+
+  onHoverLeave() {
+    this.isHover2 = false;
+    this.resetTyping();
+
+  }
+
+
+
+
+
+
+  ngOnInit(): void {
+    this.loopTypewriter();
+  }
+
+  loopTypewriter() {
+    setInterval(() => {
+      if (this.typingForward) {
+        // Type forward
+        if (this.index < this.fullText2.length) {
+          this.displayedText2 += this.fullText2[this.index];
+          this.index++;
+        } else {
+          this.typingForward = false; // start deleting
+        }
+      } else {
+        // Delete backward
+        if (this.index > 0) {
+          this.displayedText2 = this.displayedText2.slice(0, -1);
+          this.index--;
+        } else {
+          this.typingForward = true; // start typing again
+        }
+      }
+    }, this.speed);
+  }
+
+
+
+}
